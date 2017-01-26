1ef2014deb4d37b331b61dec502a88e72b40ba40
Ricky Reusser <rsreusser@gmail.com>
2017-01-25 21:37:49 -0500

    Rewrite the demo using regl

--- demo.js
+++ demo.js
@@ -1,172 +1,82 @@
-var createCamera = require('game-shell-orbit-camera')
-var createTexture = require('gl-texture2d')
-var createBuffer = require('gl-buffer')
-var createShell = require('gl-now')
-var createVAO = require('gl-vao')
+const fs = require('fs')
+const mesh = require('stanford-dragon/3')
+const textures = fs.readdirSync(__dirname + '/textures')
+const glslify = require('glslify')
+const regl = require('regl')()
+const h = require('h');
 
-var getNormals = require('normals')
-var unindex = require('unindex-mesh')
-var mat4 = require('gl-matrix').mat4
-var eye = require('eye-vector')
+const camera = require('regl-camera')(regl, {
+  distance: 2,
+  theta: 1.5,
+  phi: 0.4
+})
 
-var model = require('stanford-dragon/3')
-var matcap = require('./')
-var fs = require('fs')
 
-var textures = fs.readdirSync(__dirname + '/textures')
-var texture = document.createElement('img')
-var camera
-var shader
-var shell
-var cells
-var mesh
-var gl
-
-texture.onload = ready
-texture.src = 'textures/00001.png'
-
-function ready() {
-  shell = createShell({
-    clearColor: [0, 0, 0, 1]
+var texture, drawMesh;
+function init (img) {
+  texture = regl.texture({data: img, flipY: true})
+  drawMesh = regl({
+    vert: `
+      precision mediump float;
+      attribute vec3 position, normal;
+      uniform mat4 projection, view;
+      uniform vec3 eye;
+      varying vec3 n, e;
+      void main () {
+        n = mat3(view) * normal;
+        e = -mat3(view) * normalize(eye);
+        gl_Position = projection * view * vec4(position, 1);
+      }
+    `,
+    frag: glslify(`
+      precision mediump float;
+      #pragma glslify: matcap = require('./matcap.glsl')
+      uniform mat4 view;
+      varying vec3 n, e;
+      uniform sampler2D texture;
+      void main () {
+        vec2 uv = matcap(e, n);
+        gl_FragColor = vec4(texture2D(texture, uv).rgb, 1);
+      }
+    `),
+    uniforms: {texture: texture},
+    attributes: {
+      position: require('geom-center-and-normalize')(mesh.positions),
+      normal: require('normals').vertexNormals(mesh.cells, mesh.positions)
+    },
+    elements: mesh.cells,
+    count: mesh.cells.length * 3
   })
-
-  shell.on('gl-init', init)
-  shell.on('gl-render', render)
-
-  camera = createCamera(shell)
-  camera.distance = 150
 }
 
-function init() {
-  gl = shell.gl
+regl.frame(() => {
+  regl.clear({color: [0, 0, 0, 1], depth: 1})
+  if (drawMesh) camera(drawMesh)
+})
 
-  shader = matcap(gl)
-  texture = createTexture(gl, texture)
-  cells = createBuffer(gl, pack(model.cells))
-
-  var positions = unindex(model.positions, model.cells)
-  var normals = unindex(getNormals.vertexNormals(
-      model.cells
-    , model.positions
-  ), model.cells)
-
-  // center the model to the
-  // camera's origin
-  center(positions, 0, 3)
-  center(positions, 1, 3)
-  center(positions, 2, 3)
-
-  mesh = createVAO(gl, [{
-      size: 3
-    , buffer: createBuffer(gl, positions)
-  }, {
-      size: 3
-    , buffer: createBuffer(gl, normals)
-  }])
-
-  mesh.length = positions.length / 3
-
-  console.log('init')
-}
-
-var scratch0 = new Float32Array(16)
-var scratch1 = new Float32Array(16)
-var scratch2 = new Float32Array(16)
-var scratch3 = new Float32Array(3)
-var t = 0
-
-function render() {
-  var view = camera.view()
-
-  gl.enable(gl.DEPTH_TEST)
-  gl.enable(gl.CULL_FACE)
-
-  shader.bind()
-  shader.attributes.aPosition.location = 0
-  shader.attributes.aNormal.location = 1
-
-  shader.uniforms.uTexture = texture.bind(0)
-  shader.uniforms.uEye = eye(view, scratch3)
-
-  shader.uniforms.mView = view
-  shader.uniforms.mModel = mat4.identity(scratch1)
-  shader.uniforms.mNormal = mat4.identity(scratch2)
-  shader.uniforms.mProjection = mat4.perspective(scratch0
-    , Math.PI / 4
-    , shell.width / shell.height
-    , 0.001
-    , 1000
-  )
-
-  mesh.bind()
-  mesh.draw(gl.TRIANGLES, mesh.length)
-}
-
-function pack(arr, els) {
-  els = els || 3
-
-  var f32 = new Float32Array(arr.length * els)
-  var i = 0
-
-  for (var x = 0; x < arr.length; x++)
-  for (var y = 0; y < els; y++)
-    f32[i++] = arr[x][y]
-
-  return f32
-}
-
-function center(points, offset, stride) {
-  if ((stride = +(stride|0)) < 1) throw new Error(
-    'Stride must be equal to or greater than 1'
-  )
-
-  var l = points.length
-  var min = +Infinity
-  var max = -Infinity
-
-  for (var i = offset; i < l; i += stride) {
-    var p = points[i]
-    if (p < min) min = p
-    if (p > max) max = p
+const textureSelect = document.body.appendChild(h('div', {
+  style: {
+    left: '1em',
+    top: '1em',
+    position: 'absolute',
+    cursor: 'pointer',
+    width: '128px'
   }
-
-  var mid = min + (max - min) / 2
-
-  for (var i = offset; i < l; i += stride) {
-    points[i] -= mid
-  }
-
-  return points
-}
-
-// Switch to different textures
-var textureSelect = document.body.appendChild(
-  document.createElement('div')
-)
-
-textureSelect.style.zIndex = 9
-textureSelect.style.position = 'absolute'
-textureSelect.style.left = '1em'
-textureSelect.style.top = '1em'
-textureSelect.style.width = '128px'
-textureSelect.style.cursor = 'pointer'
+}));
 
 textureSelect.addEventListener('click', function(e) {
   var img = e.target
   if (img.nodeName !== 'IMG') return
-  if (texture) texture.dispose()
-  texture = createTexture(gl, img)
+  texture = (texture || regl.texture)({data: img, flipY: true})
 }, false)
 
 for (var i = 0; i < textures.length; i++) (function(src) {
-  var img = document.createElement('img')
-  img.onload = loaded
-  img.src = src = 'textures/' + src
+  var img = h('img', {style: {width: '32px', height: '32px', float: 'left'}});
 
-  function loaded() {
-    img.style.width = '32px'
-    img.style.height = '32px'
-    img.style.float = 'left'
+  img.onload = function () {
     textureSelect.appendChild(img)
+    if (src === '00001.png') init(img);
   }
+
+  img.src = 'textures/' + src
 }(textures[i]))
